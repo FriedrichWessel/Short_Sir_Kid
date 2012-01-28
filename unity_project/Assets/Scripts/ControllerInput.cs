6f332654919e96563d0009cea501421ff3e2650d
Friedrich Wessel <asdf-systems@Littlefoot.local>
2012-01-28 03:49:14 +0100

    added speed States

--- unity_project/Assets/Scripts/ControllerInput.cs
+++ unity_project/Assets/Scripts/ControllerInput.cs
@@ -11,7 +11,12 @@ public class ControllerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(robot.currentState == EmotionStates.States.TooFast){
+				// Can do some stuff here for not react
+		}
+		
 		if(Input.GetAxis("jump") != 0.0f){
+			
 			robot.Jump();
 		}
 		if(Input.GetAxis("hit") != 0.0f){
--- unity_project/Assets/Scripts/GameWorld.cs
+++ unity_project/Assets/Scripts/GameWorld.cs
@@ -13,7 +13,10 @@ public class GameWorld : MonoBehaviour {
 	public float SpeedStep = 0.1f;
 	public float MinSpeed = 1;
 	public float MaxSpeed = 20;
-	
+	public EmotionStates Emotions{
+		get;
+		set;
+	}
 	private float runningTime = 0.0f;
 	
 	void Awake(){
@@ -23,11 +26,12 @@ public class GameWorld : MonoBehaviour {
 		if(Instance == null){
 			Instance = this;
 		}
+		Emotions= new EmotionStates();
 	}
 	
 	void Update(){
 		if(runningTime >= CheerUpTime){
-			Debug.Log("Increase Happyness");
+			//Debug.Log("Increase Happyness");
 			RobotEntity.IncreaseHappyness();
 			runningTime = 0;
 		} else 
