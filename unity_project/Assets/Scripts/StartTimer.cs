1c0d9b6112ef540cc05385ecee6918b80bd77e49
Friedrich Wessel <asdf-systems@Littlefoot.local>
2012-01-29 12:24:43 +0100

    add winScreen, add font, improve Tutorial

--- unity_project/Assets/Scripts/StartTimer.cs
+++ unity_project/Assets/Scripts/StartTimer.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 using System.Collections;
 
 public class StartTimer : MonoBehaviour {
-
-
 	
+		public int LoadLevel = 0;
+
+
+		
 	// Use this for initialization
 	void Start () {
 		
@@ -13,6 +15,6 @@ public class StartTimer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetAxis("jump") != 0)
-			Application.LoadLevel(1);
+			Application.LoadLevel(LoadLevel);
 	}
 }
